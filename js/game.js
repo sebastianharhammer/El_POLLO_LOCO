55203dfecb643104af1a604d99f12fe3e679ed41
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-01-24 14:18:35 +0100

    implemented keyboard function

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 
 function init() {
@@ -9,6 +10,40 @@ function init() {
 }
 
 
-document.addEventListener('onkeydown', (e) => {
-    console.log(e);
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = true;
+    }
+    if (e.keyCode == 37) {
+        keyboard.LEFT = true;
+    }
+    if (e.keyCode == 38) {
+        keyboard.UP = true;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = true;
+    }
+    if (e.keyCode == 32) {
+        keyboard.SPACE = true;
+    }
+    console.log(e)
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = false;
+    }
+    if (e.keyCode == 37) {
+        keyboard.LEFT = false;
+    }
+    if (e.keyCode == 38) {
+        keyboard.UP = false;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = false;
+    }
+    if (e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+    console.log(e)
 });
