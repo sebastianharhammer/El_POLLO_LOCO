6ad365ce38bc8bb8ffa40b276dfcf9d86c862590
Sebastian Harhammer <sebastian.harhammer@gmail.com>
2025-01-24 21:52:43 +0100

    pepe can fly

--- classes/character.class.js
+++ classes/character.class.js
@@ -8,31 +8,55 @@ class Character extends MoveableObject {
     "img/2_character_pepe/2_walk/W-25.png",
     "img/2_character_pepe/2_walk/W-26.png",
   ];
+  IMAGES_JUMPING = [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+  ];
   world;
   speed = 5;
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.applyGravity();
     this.animate();
   }
   animate() {
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+        this.x += this.speed;
+        this.otherDirection = false;
+        //this.walking_sound.play();
+      }
+      if (this.world.keyboard.LEFT && this.x > 0) {
+        this.x -= this.speed;
+        this.otherDirection = true;
+        //this.walking_sound.play();
+      }
+      /* console.log('this.speedY:', this.speedY); */
+      if (this.world.keyboard.UP) {
+        this.speedY = 20;
+      }
 
-    setInterval(() =>  {
-        if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x ) {
-            this.x += this.speed;
-            this.otherDirection = false;
-        }
-        if (this.world.keyboard.LEFT && this.x > 0 ) {
-            this.x -= this.speed;
-            this.otherDirection = true;
-        }
-        this.world.camera_x = -this.x + 200;
+      this.world.camera_x = -this.x + 200;
+
     }, 1000 / 60);
 
+
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
           this.playAnimation(this.IMAGES_WALKING);
+        }
       }
     }, 50);
   }
--- classes/moveable-object.class.js
+++ classes/moveable-object.class.js
@@ -13,7 +13,7 @@ class MoveableObject {
 
     applyGravity() {
         setInterval(() => {
-            if (this.isAboveGround()) {
+            if (this.isAboveGround() || this.speedY > 0) {
             this.y -= this.speedY;
             this.speedY -= this.acceleration;
         }
