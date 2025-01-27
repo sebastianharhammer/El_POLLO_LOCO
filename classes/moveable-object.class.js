e01cefef930318e98787df9d65428bea5fa08006
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-01-27 11:46:53 +0100

    colision boxes only chicken, character

--- classes/moveable-object.class.js
+++ classes/moveable-object.class.js
@@ -39,11 +39,14 @@ class MoveableObject {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
   drawFrame(ctx) {
+
+    if (this instanceof Character || this instanceof Chicken) {
     ctx.beginPath();
     ctx.lineWidth = "5";
     ctx.strokeStyle = "blue";
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.stroke();  
+  }
   }
   playAnimation(images) {
     let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 7 % 6; => 1 Rest 1
